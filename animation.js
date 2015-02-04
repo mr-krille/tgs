146154d71e291d3968af8baa1e671f42a91139f0
Dominik Schmidt <visualcake@me.com>
2015-02-04 13:16:59 +0100

    fixed animation script

--- animation.js
+++ animation.js
@@ -17,6 +17,9 @@ var animation = {
   selectors: [],
   triggered: [],
   init: function () {
+    if (typeof(this.selectors) == undefined) {
+      this.selectors = window.selectors;
+    }
     window.addEventListener('MozMousePixelScroll', animation.handler);
     window.addEventListener('mousewheel', animation.handler);
     window.addEventListener('DOMMouseScroll', animation.handler);
@@ -26,18 +29,15 @@ var animation = {
   },
   handler: function (evt) {
     var scrollTop = window.scrollY;
-    var sels = animation.selectors;
+    var sels = window.selectors || animation.selectors;
     sels.forEach(function (opt) {
       opt = merge({ offset: 0, once: false }, opt);
       var el = document.getElementById(opt.selector);
       var topOffset = el.offsetTop - opt.offset;
-      if (!(animation.triggered.indexOf(opt.selector) > -1)) {
-        if (scrollTop > topOffset) {
-          el.classList.add('active');
-          animation.triggered << opt.selector;
-        } else if (scrollTop > (topOffset * 0.5)) {
-          el.classList.remove('active');
-        }
+      if (scrollTop > topOffset) {
+        el.classList.add('active');
+      } else if (scrollTop > (topOffset * 0.5)) {
+        el.classList.remove('active');
       }
     });
   }
