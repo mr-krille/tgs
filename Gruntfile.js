8e76dab91dc9acbba3b1e3aa9f29dbb7576ad754
Dominik Schmidt <visualcake@me.com>
2015-02-16 11:07:50 +0100

    dev server via grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,17 @@
 module.exports = function(grunt) {
   grunt.initConfig({
+    connect: {
+      server: {
+        port: 8000,
+        livereload: true,
+        base: '.'
+      }
+    },
+    open: {
+      server: {
+        path: 'http://127.0.0.1:8000'
+      }
+    },
     compass: {
       dist: {
         options: {
@@ -32,8 +44,10 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-open');
 
-  grunt.registerTask('serve', ['compass:dev', 'watch']);
+  grunt.registerTask('serve', ['compass:dev', 'connect:server', 'open:server', 'watch']);
   grunt.registerTask('build', ['compass:dist'])
   grunt.registerTask('default', ['serve']);
 };
